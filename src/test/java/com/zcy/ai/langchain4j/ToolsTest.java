30bd63dc8917051e7bf7acbeb308ab9acc345990
zhangcaoyuan <2027142766@qq.com>
2026-01-09 17:30:29 +0800

    feat: langchain4j learn ServiceWithDynamicToolsExample

--- src/test/java/com/zcy/ai/langchain4j/ToolsTest.java
+++ src/test/java/com/zcy/ai/langchain4j/ToolsTest.java
@@ -1,9 +1,16 @@
 package com.zcy.ai.langchain4j;
 
+import dev.langchain4j.code.judge0.Judge0JavaScriptExecutionTool;
 import com.zcy.ai.langchain4j.assistant.SeparateChatAssistant;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.service.AiServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static java.time.Duration.ofSeconds;
 
 @SpringBootTest
 public class ToolsTest {
@@ -11,9 +18,48 @@ public class ToolsTest {
     @Autowired
     private SeparateChatAssistant separateChatAssistant;
 
+    @Autowired
+    private OpenAiChatModel openAiChatModel;
+
     @Test
     public void testCalculatorTools(){
         String answer = separateChatAssistant.chat(6,"1+2等于几？4的平方根是几？");
         System.out.println(answer);
+    }
+
+    interface Assistant {
+
+        String chat(String message);
+    }
+
+    public static void main(String[] args) {
+
+        Judge0JavaScriptExecutionTool judge0Tool = new Judge0JavaScriptExecutionTool(ApiKeys.RAPID_API_KEY);
+
+        ChatLanguageModel chatModel = OpenAiChatModel.builder()
+                .baseUrl("")
+                .apiKey("")
+                .modelName("")
+                .temperature(0.0)
+                .timeout(ofSeconds(60))
+                .build();
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatModel(chatModel)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(20))
+                .tools(judge0Tool)
+                .build();
+
+        interact(assistant, "What is the square root of 49506838032859?");
+        interact(assistant, "Capitalize every third letter: abcabc");
+        interact(assistant, "What is the number of hours between 17:00 on 21 Feb 1988 and 04:00 on 12 Apr 2014?");
+    }
+
+    private static void interact(Assistant assistant, String userMessage) {
+        System.out.println("[User]: " + userMessage);
+        String answer = assistant.chat(userMessage);
+        System.out.println("[Assistant]: " + answer);
+        System.out.println();
+        System.out.println();
     }
 }
